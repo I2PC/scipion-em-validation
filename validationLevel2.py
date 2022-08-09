c56681071307697094ba5da6ca117ec230d6ffe6
Carlos Oscar Sorzano <coss@cnb.csic.es>
2022-08-09 09:21:30 +0200

    Bug fix in the 2D classes comparison

--- validationLevel2.py
+++ validationLevel2.py
@@ -151,7 +151,7 @@ reported resolution of the map.
     testWarnings = False
     if np.sum(np.array(cc)<0.7)/len(cc)>0.2 or testWarnings:
         warnings.append("{\\color{red} \\textbf{A large fraction of the 2D classes, %4.1f\\%%, correlate less "\
-                        "than 0.7 with reprojections of the input map}}"%(np.sum(cc<0.7)/len(cc)*100))
+                        "than 0.7 with reprojections of the input map}}"%(np.sum(np.array(cc)<0.7)/len(cc)*100))
     msg = \
 """\\textbf{Automatic criteria}: The validation is OK if the proportion of classes for which the correlation is below
 0.7 is smaller than 20\\%.
